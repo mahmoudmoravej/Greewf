cacc29b84bc2d36ce340f2ba2d33f6cd35fadc56
mahmoud690 <mahmoud690@yahoo.com>
2019-02-27 19:55:05 +0330

    Upgrade to c#7.3 + add some functionalities related to Conditions

--- Greewf.BaseLibrary/Source/Greewf.BaseLibrary/Security/V3/PermissionCoordinatorBase.cs
+++ Greewf.BaseLibrary/Source/Greewf.BaseLibrary/Security/V3/PermissionCoordinatorBase.cs
@@ -45,38 +45,26 @@ namespace Greewf.BaseLibrary.Security.V3
     {
 
 
-        private List<RelatedPermission<G>> lstRelatedPermissions;
         private Dictionary<Type, G> dicTypeGroupMaps;
         private Dictionary<G, OT> dicGroupObjectTypeMaps;
         private Dictionary<OT, OT[]> dicObjectTypeParents;
+        private List<object> dicConditionalPermissions;
 
         protected PermissionCoordinatorBase()
         {
             dicGroupObjectTypeMaps = LoadGroupObjectTypeMaps() ?? new Dictionary<G, OT>();
             dicTypeGroupMaps = LoadTypeGroupMaps() ?? new Dictionary<Type, G>();
             dicObjectTypeParents = LoadObjectTypeParents() ?? new Dictionary<OT, OT[]>();
-            lstRelatedPermissions = LoadRelationships() ?? new List<RelatedPermission<G>>();
-
+            dicConditionalPermissions = LoadConditionalPermissions() ?? new List<object>();
         }
 
-        //private static PermissionCoordinatorBase<G, OT> _instance = null;
-        //public static T GetInstance<T>() where T : PermissionCoordinatorBase<G, OT>, new()
-        //{
-        //    if (_instance == null)
-        //        _instance = new T();
+        #region Permission/Object Relationship
 
-        //    var result = _instance as T;
-        //    if (result == null)
-        //        throw new Exception(string.Format("This method can be called only with the type that was passed to it first call. It is associated with '{0}' previously." + typeof(T).FullName));
-
-        //    return result;
-        //}
-
-        protected abstract List<RelatedPermission<G>> LoadRelationships();
         protected abstract Dictionary<Type, G> LoadTypeGroupMaps();
         protected abstract Dictionary<G, OT> LoadGroupObjectTypeMaps();
         protected abstract Dictionary<OT, OT[]> LoadObjectTypeParents();
 
+        protected abstract List<object> LoadConditionalPermissions();
 
         public G GetRelatedPermissionItem(Type type)
         {
@@ -88,29 +76,6 @@ namespace Greewf.BaseLibrary.Security.V3
             {
                 throw new Exception(string.Format("The given type({0}) is not mapped in PermissionCoordinator's 'LoadPermissionRelationships' method(for 'enumMaps' parameter).", type.ToString()));
             }
-        }
-
-
-        /// <summary>
-        /// آیا فقط اجازه جزیی را دارد و اجازه کامل آنرا ندارد؟
-        /// </summary>
-        /// <param name="group"></param>
-        /// <param name="requestedPermission"></param>
-        /// <param name="userPermissions"></param>
-        /// <returns></returns>
-        public bool HasOnlyExclusive(G group, long requestedPermission, long userPermissions)
-        {
-            //اجازه جزیی : ViewOwn
-            //اجازه کامل : ViewFull
-
-            // اگر کاربر فقط اجازه جزیی را دارد ولی کلی را ندارد و درخواست اجازه هم برای فقط جزیی بود آنگاه باید کاربر چک شود
-            foreach (var item in lstRelatedPermissions.Where(o => o.Group.Equals(group)))
-                if ((requestedPermission & item.Exclusive) != 0)//اگر اجازه درخواستی حاوی اجازه ی جزیی ای است
-                    if ((userPermissions & (item.Exclusive | item.Inclusive)) == item.Exclusive)//اگر اجازه های کاربر فقط اجازه جزیی را دارد و اجازه کامل آنرا شامل نمی شود
-                        return true;
-
-            return false;
-
         }
 
         public OT GetGroupObjectType(G group)
@@ -143,6 +108,25 @@ namespace Greewf.BaseLibrary.Security.V3
         {
             return dicObjectTypeParents.Where(o => o.Value.Contains(objectType)).Select(o => o.Key).ToArray();
         }
+
+        
+        public List<object> GetConditionalPermissions()
+        {
+            return dicConditionalPermissions;
+        }
+
+        public (G PermissionGroup, long PermissionFlagId) GetPermissionInfo<P>(P permission) where P : System.Enum
+        {
+            var group = GetGroupOfType(permission.GetType());            
+            return (group, Convert.ToInt64(permission));
+
+        }
+
+
+
+        #endregion
+
+        #region HasPermission methods        
 
         public abstract Task<bool> HasPermissionAsync(int userId, PermissionObject<OT>? obj, G group, long permission, string[] conditions = null);
 
@@ -184,6 +168,10 @@ namespace Greewf.BaseLibrary.Security.V3
             return HasPermission(userId, PermissionObject<OT>.ReadFrom(obj), group, permission, conditions);
         }
 
+        #endregion
+
+        #region Get Allowed lists
+
         /// <summary>
         /// 
         /// </summary>
@@ -224,6 +212,9 @@ namespace Greewf.BaseLibrary.Security.V3
         }
 
         protected abstract int[] GetAllowedUserIds(PermissionObject<OT> obj, int group, long permission, string[] conditions = null);
+
+
+        #endregion
 
     }
 }
