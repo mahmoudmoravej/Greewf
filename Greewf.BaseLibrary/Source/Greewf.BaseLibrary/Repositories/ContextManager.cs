b0efd08958b0636b4ef80521f8f5b4ccf6cfd5e8
mahmoud690 <mahmoud690@yahoo.com>
2019-03-12 18:27:09 +0330

    savechangesasync

--- Greewf.BaseLibrary/Source/Greewf.BaseLibrary/Repositories/ContextManager.cs
+++ Greewf.BaseLibrary/Source/Greewf.BaseLibrary/Repositories/ContextManager.cs
@@ -38,17 +38,9 @@ namespace Greewf.BaseLibrary.Repositories
             }
         }
 
+        //NOTE!!!!!!!!!!!!!! تغییرات را در هر دو متد یکسان کنید
+        //با توجه به توضیح داده شده در اینجا، دو ورژن نوشتمی : https://stackoverflow.com/a/14876375/790811
         public virtual bool SaveChanges()
-        {
-            return SaveChangesBase(() => Context.SaveChanges());
-        }
-
-        public virtual Task<bool> SaveChangesAsync()
-        {
-            return Task.FromResult(SaveChangesBase(() => Context.SaveChangesAsync()));
-        }
-
-        private bool SaveChangesBase(Action contextSaveChangesFunction)
         {
             if (ValidationDictionary != null && !ValidationDictionary.IsValid)
                 return false;
@@ -58,9 +50,13 @@ namespace Greewf.BaseLibrary.Repositories
             //to avoid creating new scope (it comes when savechanges causes to call savechanges again). 
             //شرط دوم برای این است که ممکن است کانتکست از ابتدا با یک کانکشنی ایجاد شده باشد که خود در درون ترنزکشن است
             //در این حالت ایجاد ترنزکشن اسکوپ باعث می شود خطای روبرو را بگیریم : "Cannot enlist in the transaction because a local transaction is in progress on the connection"
+            //همچنین توجه کنید نیازی به فراخوانی های ایونت ها نیست. چراکه در واقع مانند یک کانتکست معمولی با ان برخورد می شود
+            //و پس از فراخوانی ذخیره، خود کانتکست چون می داند در اسکوپ نیست، ایونت کامیت را فراخوانی می کند
+            //و ازآنجا که این ایونت مختص زمانیکه فراخوانی می شود که از کامیت خود مطمن باشد صحیح هست. حال مهم نیست که 
+            //تراکنشی بیرون تر آنرا دارد مدیریت می کند یا خیر
             if (Context.IsInActiveTransactionScope || Context.Database.CurrentTransaction != null)
             {
-                contextSaveChangesFunction();
+                Context.SaveChanges();
                 if (ValidationDictionary?.IsValid == false)
                     result = false;
 
@@ -71,10 +67,11 @@ namespace Greewf.BaseLibrary.Repositories
             {
                 Context.OnBeforeTransactionScopeStart();
 
+                Context.Database.Connection.Close();
                 using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled/*due to https://stackoverflow.com/q/13543254/790811 to be able to support async cases*/ ))
                 {
                     Context.IsInActiveTransactionScope = true;
-                    contextSaveChangesFunction();//my be some calls on onChangesSaved event that needs to be in transaction
+                     Context.SaveChanges();//my be some calls on onChangesSaved event that needs to be in transaction
 
                     if (ValidationDictionary?.IsValid == false)
                         result = false;
@@ -99,6 +96,67 @@ namespace Greewf.BaseLibrary.Repositories
 
             return result;
         }
+
+        //NOTE!!!!!!!!!!!!!! تغییرات را در هر دو متد یکسان کنید
+        //با توجه به توضیح داده شده در اینجا، دو ورژن نوشتمی : https://stackoverflow.com/a/14876375/790811
+        public virtual async Task<bool> SaveChangesAsync()
+        {
+            if (ValidationDictionary != null && !ValidationDictionary.IsValid)
+                return false;
+
+            bool result = true;
+
+            //to avoid creating new scope (it comes when savechanges causes to call savechanges again). 
+            //شرط دوم برای این است که ممکن است کانتکست از ابتدا با یک کانکشنی ایجاد شده باشد که خود در درون ترنزکشن است
+            //در این حالت ایجاد ترنزکشن اسکوپ باعث می شود خطای روبرو را بگیریم : "Cannot enlist in the transaction because a local transaction is in progress on the connection"
+            //همچنین توجه کنید نیازی به فراخوانی های ایونت ها نیست. چراکه در واقع مانند یک کانتکست معمولی با ان برخورد می شود
+            //و پس از فراخوانی ذخیره، خود کانتکست چون می داند در اسکوپ نیست، ایونت کامیت را فراخوانی می کند
+            //و ازآنجا که این ایونت مختص زمانیکه فراخوانی می شود که از کامیت خود مطمن باشد صحیح هست. حال مهم نیست که 
+            //تراکنشی بیرون تر آنرا دارد مدیریت می کند یا خیر
+            if (Context.IsInActiveTransactionScope || Context.Database.CurrentTransaction != null)
+            {
+                await Context.SaveChangesAsync();
+                if (ValidationDictionary?.IsValid == false)
+                    result = false;
+
+                return result;
+            }
+
+            try
+            {
+                Context.OnBeforeTransactionScopeStart();
+
+                Context.Database.Connection.Close();
+                using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled/*due to https://stackoverflow.com/q/13543254/790811 to be able to support async cases*/ ))
+                {
+                    Context.IsInActiveTransactionScope = true;
+                    await Context.SaveChangesAsync();//my be some calls on onChangesSaved event that needs to be in transaction
+
+                    if (ValidationDictionary?.IsValid == false)
+                        result = false;
+
+                    if (result)
+                        scope.Complete();
+                    else
+                        scope.Dispose();
+                }
+            }
+            finally
+            {
+                Context.IsInActiveTransactionScope = false;
+            }
+
+
+
+            if (result)
+                Context.OnTransactionScopeCommitted();
+            else
+                Context.OnTransactionScopeRollbacked();
+
+            return result;
+        }
+
+  
 
     }
 }
